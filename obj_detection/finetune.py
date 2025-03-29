5a987ab5cc13a8b75b1102fea40e332940582163
owen <udnewo@gmail.com>
2025-03-29 14:36:18 +0100

    started yolo finetune code

---
+++ obj_detection/finetune.py
@@ -0,0 +1,24 @@
+from ultralytics import YOLO
+import wandb
+
+# Load a pretrained YOLOv9 model
+model = YOLO("yolov9c.pt")
+model_path = "yolov9c_finetunedv2.pt"
+# Train the model on your datasetfine
+results = model.train(
+    data="./data.yaml",
+    epochs=100,
+    imgsz=640,
+    project="yolo_training",  # wandb project name
+    name="yolov9_finetune",     # wandb run name
+    device="0"
+)
+
+
+# Save the fine-tuned model
+model.save(model_path)
+
+
+wandb.init("yolo_training")
+wandb.log_model(model_path)
+wandb.finish()
